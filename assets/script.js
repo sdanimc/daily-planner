cdaf3ce9c8ef38976608785de7b5e68f08476e60
sdanimc <116037382+sdanimc@users.noreply.github.com>
2022-11-17 15:07:31 -0800

    organized and commented code

--- assets/script.js
+++ assets/script.js
@@ -1,21 +1,17 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-var pageHeader = $('.p-5');
-var dayContainer = $('.container-lg');
-var saveBtns = $('.saveBtn');
-var today = dayjs();
-console.log(today);
-var currentHour = dayjs().hour();
-var weekDay = today.format('dddd');
-var month = today.format('MMMM');
-var dayDate = today.date();
-
-$(function () {
+$(document).ready(function () {
+  //global variables
+  var pageHeader = $('.p-5');
+  var dayContainer = $('.container-lg');
+  var timeBlocks = dayContainer.children('.time-block')
+  var saveBtns = $('.saveBtn');
+  var today = dayjs();
+  var currentHour = dayjs().hour();
+  var weekDay = today.format('dddd');
+  var month = today.format('MMMM');
+  var dayDate = today.date();
   //display date
   pageHeader.children('#currentDay').text(weekDay + ", " + month + " " + dayDate);
   //color code time blocks
-  var timeBlocks = dayContainer.children('.time-block')
   for (i = 0; i < timeBlocks.length; i += 1) {
     var timeBlock = timeBlocks.eq(i)
     var blockTime = 9 + i;
@@ -43,7 +39,7 @@ $(function () {
     window.localStorage.setItem(parentId, text);
   }
   );
-  //retrieve saved texts
+  //retrieve saved text
   for (i = 0; i < timeBlocks.length; i += 1) {
     var timeBlock = timeBlocks.eq(i);
     var savedText = localStorage.getItem(timeBlock.attr('id'));
