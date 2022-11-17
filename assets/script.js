52a9cd2ae3c0b1f7d241d80df68ced0fb95367d1
sdanimc <116037382+sdanimc@users.noreply.github.com>
2022-11-17 13:04:43 -0800

    added js to get and display date and color code time blocks

--- assets/script.js
+++ assets/script.js
@@ -1,27 +1,60 @@
 // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
 // the code isn't run until the browser has finished rendering all the elements
 // in the html.
+//is below right?
+var pageHeader = $('.p-5');
+var dayContainer = $('.container-lg');
+//var timeBlocks = $('.time-block');
+var saveBtns = $('.saveBtn');
+var today = dayjs();
+console.log(today);
+var currentHour = dayjs().hour();
+var weekDay = today.format('dddd');
+var month = today.format('MMMM');
+var dayDate = today.date();
+
 $(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
+  //display date
+  pageHeader.children('#currentDay').text(weekDay + ", " + month + " " + dayDate);
+  //color code time blocks
+  var timeBlocks = dayContainer.children('.time-block')
+  for (i = 0; i < timeBlocks.length; i += 1) {
+    var timeBlock = timeBlocks.eq(i)
+    var blockTime = 9 + i;
+    if (blockTime > currentHour) {
+      timeBlock.removeClass('present');
+      timeBlock.removeClass('past');
+      timeBlock.addClass('future');
+    }
+    else if (blockTime < currentHour) {
+      timeBlock.removeClass('present');
+      timeBlock.removeClass('future');
+      timeBlock.addClass('past');
+    }
+    else {
+      timeBlock.removeClass('future');
+      timeBlock.removeClass('past');
+      timeBlock.addClass('present');
+    };
+  };
+  /* // TODO: Add a listener for click events on the save button. This code should
+   // use the id in the containing time-block as a key to save the user input in
+   // local storage. HINT: What does `this` reference in the click listener
+   // function? How can DOM traversal be used to get the "hour-x" id of the
+   // time-block containing the button that was clicked? How might the id be
+   // useful when saving the description in local storage?
+  saveBtns.on('click, function(event?)){
+    getparent attribute
+   $('#)
+   }
+  
+   */
+
   //
   // TODO: Add code to get any user input that was saved in localStorage and set
   // the values of the corresponding textarea elements. HINT: How can the id
   // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page.
 });
 
-//function for get time and date-> display day attribute to p #currentDay .lead; use time in if statement and for loop to color code blocks
-//enter 
-//save to local storage
+
+
